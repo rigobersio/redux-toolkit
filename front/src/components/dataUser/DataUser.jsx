4f6c2ae123cbf7424131a2d3e7535d9da50b3244
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-06-16 00:32:01 -0400

    (ryzen)edit:DataUser(importo user de userSlice del estado global con el hook useSelector)

--- front/src/components/dataUser/DataUser.jsx
+++ front/src/components/dataUser/DataUser.jsx
@@ -1,24 +1,27 @@
-const DataUser = () => {
+import { useSelector } from 'react-redux'; // hook para acceder al estado global y traer lo que interesa
 
-    return (
-        <header className="text-3xl font-bold underline">
-            <h2 className="text-3xl font-bold underline">Datos del usuario en store</h2>
-            <ul className="text-3xl font-bold underline">
-                <li className="text-3xl font-bold underline">
-                    Id:
-                </li>
-                <li className="text-3xl font-bold underline">
-                    Nombre: 
-                </li>
-                <li className="text-3xl font-bold underline">
-                    Nombre de usuario: 
-                </li>
-                <li className="text-3xl font-bold underline">
-                    Email: 
-                </li>
-            </ul>
-        </header>
-    )
+const DataUser = () => {
+  const user = useSelector((state) => state.user); // **state** es el estado global y **user** es el nombre que se le coloco al userSlice
+
+  return (
+    <header className="text-3xl font-bold underline">
+      <h2 className="text-3xl font-bold underline">Datos del usuario en store</h2>
+      <ul className="text-3xl font-bold underline">
+        <li className="text-3xl font-bold underline">
+          Id: {user.id}
+        </li>
+        <li className="text-3xl font-bold underline">
+          Nombre: {user.name}
+        </li>
+        <li className="text-3xl font-bold underline">
+          Nombre de usuario: {user.userName}
+        </li>
+        <li className="text-3xl font-bold underline">
+          Email: {user.email}
+        </li>
+      </ul>
+    </header>
+  )
 }
 
 export default DataUser;
